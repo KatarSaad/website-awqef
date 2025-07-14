2045cd45f79e1262de60ca667551eee9ffb12fce
Saad Katar Katar <katarsaad@gmail.com>
2025-07-14 14:40:53 +0100

    feat: deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const path = require("path"); // <-- You need this!
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["images.unsplash.com"],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
   },
 };
 
